062d73f7c60310e7842ea91409c79c108b9da298
Guilherme Melo <gldmelo@gmail.com>
2024-02-27 19:31:39 -0300

    feat: inclusão do código do Lab 03 sobre Análise de Sentimento utilizando o Azure AI Language

---
+++ lab-03-analise-sentimento/AnaliseSentimento/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+
+string endpoint = Environment.GetEnvironmentVariable("LANGUAGE_ENDPOINT")!;
+string key = Environment.GetEnvironmentVariable("LANGUAGE_KEY")!;
+
+try
+{
+    TextAnalyticsClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+
+    string textoParaAnalise = args[0];
+
+    Response<DocumentSentiment> response = client.AnalyzeSentiment(textoParaAnalise, "pt-BR");
+    DocumentSentiment docSentiment = response.Value;
+
+    var sentimento = docSentiment.Sentiment switch
+    {
+        TextSentiment.Positive => "Positivo",
+        TextSentiment.Negative => "Negativo",
+        _ => "Neutro"
+    };
+
+    Console.WriteLine($"Sentimento do texto é: { sentimento }");
+    Console.WriteLine($"Score de confiança Positivo: { docSentiment.ConfidenceScores.Positive }");
+    Console.WriteLine($"Score de confiança Neutro: { docSentiment.ConfidenceScores.Neutral }");
+    Console.WriteLine($"Score de confiança Negativo: { docSentiment.ConfidenceScores.Negative }");
+}
+catch (RequestFailedException ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
